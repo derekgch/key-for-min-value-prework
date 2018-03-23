120a6f1fe2787c50f07f7dd6ebb886890ebcf114
derek chen <derek.g.chen@gmail.com>
2018-03-23 16:47:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   min = nil
   min_v = 0
-  name_hash.each |key, vaule| do
+  name_hash.each |key, value| do
     if(vaule < min_v) do
       min = key
-      min_v = vaule
+      min_v = value
     end
   end
 
